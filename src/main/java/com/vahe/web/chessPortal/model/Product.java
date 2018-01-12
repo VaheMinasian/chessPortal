2057f2e8fe9c84a7beadd61392cd4f8ea3615370
Vahe Minasian <vahe_minasian@gmx.com>
2018-01-12 11:51:56 +0100

    removed database related components and API

--- src/main/java/com/vahe/web/chessPortal/model/Product.java
+++ src/main/java/com/vahe/web/chessPortal/model/Product.java
@@ -1,29 +1,20 @@
 package com.vahe.web.chessPortal.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+public class Product {
 
-@Entity
-public class Chess {
-
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	@NotNull @Size(min=1, max=100)
+	
 	private String name;
-	@NotNull @Size(min=1, max=100)
+	
 	private String made;
-	@NotNull @Size(min=20, max=60)
+	
 	private Integer dimension;
-	@Size(min=5)
+
 	private Float price;
 	
-	public Chess() {}
+	public Product() {}
 	
-	public Chess(String name, String made, Float price, Integer dimension) {
+	public Product(String name, String made, Float price, Integer dimension) {
         this.name = name;
         this.made = made;
         this.price = price;
--- src/main/java/com/vahe/web/chessPortal/util/TextUtil.java
+++
@@ -1,8 +0,0 @@
-package com.vahe.web.chessPortal.util;
-
-public class TextUtil {
-
-	public String sanitize(String textToSanitize) {
-        return textToSanitize.replaceAll("\\s+", " ");
-    }
-}
